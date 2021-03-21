c856e62ba35f06640c6bfc8c8ae5771928fc07ab
Patrice Stampfli <patricestampfli@patrices-macbook-pro.home>
2021-03-21 14:24:24 +0100

    begin with implementation

---
+++ client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+i = 0
+client.connect((HOST, PORT))
+while i <= 10:
+    client.sendall(b"Ping")
+    i = i + 1
+
+
+
+
---
+++ server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+def main():
+    print(f'Creating Socket {HOST}, {PORT}')
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server.bind((HOST, PORT))
+    print("Socket is listening")
+    server.listen()
+    while True:
+        print("Waiting for a client ....")
+        (client, address) = server.accept()
+        print(client)
+
+
+if __name__ == "__main__":
+    main()
